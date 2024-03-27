e6a739dbcfe8128273141fd7d6fd0612686fe043
Gleb Voitenko <v8tenko@gmail.com>
2024-03-27 17:01:14 +0300

    chore: initialize

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ['@diplodoc/eslint-config', '@diplodoc/eslint-config/prettier'],
+  root: true,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+};
